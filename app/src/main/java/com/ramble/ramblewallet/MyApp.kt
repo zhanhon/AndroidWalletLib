72c480fce510d496a53079ed64b91b425f7eed7b
potatoph8 <potatoph2017@gmail.com>
2021-12-15 09:58:04 +0800

    增加项目应用程序类，初始化友盟SDK

---
+++ app/src/main/java/com/ramble/ramblewallet/MyApp.kt
@@ -0,0 +1,43 @@
+package com.ramble.ramblewallet
+
+import android.app.Application
+import com.ramble.ramblewallet.helper.PushHelper
+import com.umeng.commonsdk.UMConfigure
+import com.umeng.commonsdk.utils.UMUtils
+
+/**
+ * 时间　: 2021/12/15 9:50
+ * 作者　: potato
+ * 描述　:应用程序类
+ */
+class MyApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initUmengSDK()
+    }
+
+    /**
+     * 初始化友盟SDK
+     */
+    private fun initUmengSDK() {
+        //日志开关
+        UMConfigure.setLogEnabled(true)
+        //预初始化
+        PushHelper.preInit(this)
+        //是否同意隐私政策
+//        val agreed: Boolean = MyPreferences.getInstance(this).hasAgreePrivacyAgreement()
+//        if (!agreed) {
+//            return
+//        }
+        val isMainProcess = UMUtils.isMainProgress(this)
+        if (isMainProcess) {
+            //启动优化：建议在子线程中执行初始化
+            Thread { PushHelper.init(applicationContext) }.start()
+        } else {
+            //若不是主进程（":channel"结尾的进程），直接初始化sdk，不可在子线程中执行
+            PushHelper.init(applicationContext)
+        }
+    }
+
+}
