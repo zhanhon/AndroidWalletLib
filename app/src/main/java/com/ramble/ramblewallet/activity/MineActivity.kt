dc2a2a83a1bd21ea6e1273573f8907655d970f78
potatoph8 <potatoph2017@gmail.com>
2021-12-16 18:40:45 +0800

    我的管理页面优化

--- app/src/main/java/com/ramble/ramblewallet/activity/MineActivity.kt
+++ app/src/main/java/com/ramble/ramblewallet/activity/MineActivity.kt
@@ -1,8 +1,9 @@
 package com.ramble.ramblewallet.activity
 
-import android.content.Intent
+
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
@@ -14,9 +15,11 @@ import com.ramble.ramblewallet.constant.ARG_PARAM3
 import com.ramble.ramblewallet.constant.ARG_PARAM4
 import com.ramble.ramblewallet.databinding.ActivityMineBinding
 import com.ramble.ramblewallet.helper.start
-import com.ramble.ramblewallet.helper.start2
 
-class MineActivity : BaseActivity() {
+/***
+ * 我的管理页面
+ */
+class MineActivity : BaseActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMineBinding
 
@@ -24,14 +27,13 @@ class MineActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_mine)
-
-        binding.ivBack.setOnClickListener {
-            finish()
-        }
-        binding.ivMineRight.setOnClickListener {
-            start(MessageCenterActivity::class.java)
-        }
-
+        initView()
+        initListener()
+    }
+    /***
+     * View初始化
+     */
+    private fun initView() {
         binding.tvMineTitle.text = getString(R.string.personal_management)
         binding.incManageWallet.findViewById<TextView>(R.id.tv_mine_title).text =
             getString(R.string.manage_wallet)
@@ -51,35 +53,55 @@ class MineActivity : BaseActivity() {
             getString(R.string.privacy_statement)
         binding.incAboutUs.findViewById<TextView>(R.id.tv_mine_title).text =
             getString(R.string.about_us)
+    }
+    /****
+     * 事件监听
+     */
+    private fun initListener() {
+        binding.ivBack.setOnClickListener(this)
+        binding.ivMineRight.setOnClickListener(this)
+        binding.incServiceAgreement.setOnClickListener(this)
+        binding.incPrivacyStatement.setOnClickListener(this)
+        binding.incManageWallet.setOnClickListener(this)
+        binding.incAddressBook.setOnClickListener(this)
 
-        binding.incServiceAgreement.setOnClickListener {
-            start(MsgDetailsActivity::class.java, Bundle().also {
-                it.putString(ARG_PARAM1, getString(R.string.service_agreement))
-                it.putString(
-                    ARG_PARAM2,
-                    "我是服务协议，我要打10个，我是服务协议，我要打10个，我是服务协议，我要打10个，我是服务协议，我要打10个，我是服务协议，我要打10个，我是服务协议，我要打10个"
-                )
-                it.putString(ARG_PARAM3, "1111111111111111111111111")
-                it.putString(ARG_PARAM4, getString(R.string.service_agreement))
-            })
-        }
+    }
 
-        binding.incPrivacyStatement.setOnClickListener {
-            start(MsgDetailsActivity::class.java, Bundle().also {
-                it.putString(ARG_PARAM1, getString(R.string.privacy_statement))
-                it.putString(
-                    ARG_PARAM2,
-                    "我是隐私声明，我要打10个，我是隐私声明，我要打10个，我是隐私声明，我要打10个，我是隐私声明，我要打10个，我是隐私声明，我要打10个，我是隐私声明，我要打10个"
-                )
-                it.putString(ARG_PARAM3, "1111111111111111111111111")
-                it.putString(ARG_PARAM4, getString(R.string.privacy_statement))
-            })
-        }
-        binding.incManageWallet.setOnClickListener {
-            startActivity(Intent(this, ManageWalletActivity::class.java))
-        }
-        binding.incAddressBook.setOnClickListener {
-            startActivity(Intent(this, AddressBookActivity::class.java))
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.iv_back -> finish()
+            R.id.iv_mine_right -> {
+                start(MessageCenterActivity::class.java)
+            }
+            R.id.inc_service_agreement -> {
+                start(MsgDetailsActivity::class.java, Bundle().also {
+                    it.putString(ARG_PARAM1, getString(R.string.service_agreement))
+                    it.putString(
+                        ARG_PARAM2,
+                        "我是服务协议，我要打10个，我是服务协议，我要打10个，我是服务协议，我要打10个，我是服务协议，我要打10个，我是服务协议，我要打10个，我是服务协议，我要打10个"
+                    )
+                    it.putString(ARG_PARAM3, "1111111111111111111111111")
+                    it.putString(ARG_PARAM4, getString(R.string.service_agreement))
+                })
+            }
+            R.id.inc_privacy_statement -> {
+                start(MsgDetailsActivity::class.java, Bundle().also {
+                    it.putString(ARG_PARAM1, getString(R.string.privacy_statement))
+                    it.putString(
+                        ARG_PARAM2,
+                        "我是隐私声明，我要打10个，我是隐私声明，我要打10个，我是隐私声明，我要打10个，我是隐私声明，我要打10个，我是隐私声明，我要打10个，我是隐私声明，我要打10个"
+                    )
+                    it.putString(ARG_PARAM3, "1111111111111111111111111")
+                    it.putString(ARG_PARAM4, getString(R.string.privacy_statement))
+                })
+            }
+            R.id.inc_manage_wallet -> {
+                start(ManageWalletActivity::class.java)
+            }
+            R.id.inc_address_book -> {
+                start(AddressBookActivity::class.java)
+            }
         }
     }
 }
