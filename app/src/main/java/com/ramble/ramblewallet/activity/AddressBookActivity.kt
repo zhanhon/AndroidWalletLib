59b4f108acfbe4331a400d798bca318ecd362934
potatoph8 <potatoph2017@gmail.com>
2021-12-28 19:43:30 +0800

    地址页逻辑1

--- app/src/main/java/com/ramble/ramblewallet/activity/AddressBookActivity.kt
+++ app/src/main/java/com/ramble/ramblewallet/activity/AddressBookActivity.kt
@@ -3,30 +3,30 @@ package com.ramble.ramblewallet.activity
 import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.widget.RadioButton
 import android.widget.RadioGroup
 import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.OrientationHelper
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.listener.OnItemChildClickListener
+import com.chad.library.adapter.base.listener.OnItemClickListener
 import com.ramble.ramblewallet.R
 import com.ramble.ramblewallet.adapter.AddressBookAdapter
-import com.ramble.ramblewallet.adapter.AddressBookIconAdapter
 import com.ramble.ramblewallet.base.BaseActivity
 import com.ramble.ramblewallet.bean.MyAddressBean
-import com.ramble.ramblewallet.bean.MyDataBean
 import com.ramble.ramblewallet.databinding.ActivityAddressBookBinding
-import com.ramble.ramblewallet.helper.start2
 import com.ramble.ramblewallet.item.TransferItem
 import com.ramble.ramblewallet.wight.adapter.AdapterUtils
 
-class AddressBookActivity : BaseActivity() , RadioGroup.OnCheckedChangeListener,View.OnClickListener{
+class AddressBookActivity : BaseActivity(), RadioGroup.OnCheckedChangeListener,
+    View.OnClickListener  {
 
     private lateinit var binding: ActivityAddressBookBinding
     private var myDataBeans: ArrayList<MyAddressBean> = arrayListOf()
     private lateinit var addressBookAdapter: AddressBookAdapter
+    private var isDelete=false
 
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
@@ -46,19 +46,19 @@ class AddressBookActivity : BaseActivity() , RadioGroup.OnCheckedChangeListener,
         val linearLayoutManager = LinearLayoutManager(this)
         linearLayoutManager.orientation = OrientationHelper.VERTICAL
         binding.rvMainCurrency.layoutManager = linearLayoutManager
-        myDataBeans=arrayListOf()
-        var a=MyAddressBean()
-        a.userName="大爷"
-        a.address="dfgfdfgpfdgkdpdsfs"
-        a.type=1
-        var a1=MyAddressBean()
-        a1.userName="大爷1"
-        a1.address="dfgfdfgpfdgkdpdsfs1"
-        a1.type=2
-        var a2=MyAddressBean()
-        a2.userName="大爷2"
-        a2.address="dfgfdfgpfdgkdpdsfs2"
-        a2.type=3
+        myDataBeans = arrayListOf()
+        var a = MyAddressBean()
+        a.userName = "大爷"
+        a.address = "dfgfdfgpfdgkdpdsfs"
+        a.type = 1
+        var a1 = MyAddressBean()
+        a1.userName = "大爷1"
+        a1.address = "dfgfdfgpfdgkdpdsfs1"
+        a1.type = 2
+        var a2 = MyAddressBean()
+        a2.userName = "大爷2"
+        a2.address = "dfgfdfgpfdgkdpdsfs2"
+        a2.type = 3
         myDataBeans.add(a)
         myDataBeans.add(a1)
         myDataBeans.add(a2)
@@ -70,6 +70,23 @@ class AddressBookActivity : BaseActivity() , RadioGroup.OnCheckedChangeListener,
         binding.ivBack.setOnClickListener(this)
         binding.delete.setOnClickListener(this)
         binding.add.setOnClickListener(this)
+        addressBookAdapter.addChildClickViewIds(R.id.iv_reduce)
+        addressBookAdapter.setOnItemChildClickListener(OnItemChildClickListener { adapter, view, position ->
+//            when(view.id){
+//                R.id.iv_menu->{
+//
+//                }
+//                R.id.iv_reduce->{
+//                    println("-=-=-=->sav撒旦发射点发射点发范德萨发撒是否")
+//                    myDataBeans.removeAt(position)
+//                    addressBookAdapter = AddressBookAdapter(myDataBeans, false)
+//                    binding.rvMainCurrency.adapter = addressBookAdapter
+//                    binding.rvMainCurrency.adapter!!.notifyDataSetChanged()
+//                }
+//            }
+            println("-=-=-=->11111111111111111111")
+        })
+
     }
 
     private fun loadData() {
@@ -80,76 +97,76 @@ class AddressBookActivity : BaseActivity() , RadioGroup.OnCheckedChangeListener,
     override fun onCheckedChanged(group: RadioGroup?, checkedId: Int) {
         when (checkedId) {
             R.id.check_all -> {
-                myDataBeans=arrayListOf()
-                var a=MyAddressBean()
-                a.userName="大爷"
-                a.address="dfgfdfgpfdgkdpdsfs"
-                a.type=1
-                var a1=MyAddressBean()
-                a1.userName="大爷1"
-                a1.address="dfgfdfgpfdgkdpdsfs1"
-                a1.type=2
-                var a2=MyAddressBean()
-                a2.userName="大爷2"
-                a2.address="dfgfdfgpfdgkdpdsfs2"
-                a2.type=3
+                myDataBeans = arrayListOf()
+                var a = MyAddressBean()
+                a.userName = "大爷"
+                a.address = "dfgfdfgpfdgkdpdsfs"
+                a.type = 1
+                var a1 = MyAddressBean()
+                a1.userName = "大爷1"
+                a1.address = "dfgfdfgpfdgkdpdsfs1"
+                a1.type = 2
+                var a2 = MyAddressBean()
+                a2.userName = "大爷2"
+                a2.address = "dfgfdfgpfdgkdpdsfs2"
+                a2.type = 3
                 myDataBeans.add(a)
                 myDataBeans.add(a1)
                 myDataBeans.add(a2)
                 loadData()
             }
             R.id.check_bt -> {
-                myDataBeans=arrayListOf()
-                var a=MyAddressBean()
-                a.userName="大爷"
-                a.address="dfgfdfgpfdgkdpdsfs"
-                a.type=2
-                var a1=MyAddressBean()
-                a1.userName="大爷1"
-                a1.address="dfgfdfgpfdgkdpdsfs1"
-                a1.type=2
-                var a2=MyAddressBean()
-                a2.userName="大爷2"
-                a2.address="dfgfdfgpfdgkdpdsfs2"
-                a2.type=2
+                myDataBeans = arrayListOf()
+                var a = MyAddressBean()
+                a.userName = "大爷"
+                a.address = "dfgfdfgpfdgkdpdsfs"
+                a.type = 2
+                var a1 = MyAddressBean()
+                a1.userName = "大爷1"
+                a1.address = "dfgfdfgpfdgkdpdsfs1"
+                a1.type = 2
+                var a2 = MyAddressBean()
+                a2.userName = "大爷2"
+                a2.address = "dfgfdfgpfdgkdpdsfs2"
+                a2.type = 2
                 myDataBeans.add(a)
                 myDataBeans.add(a1)
                 myDataBeans.add(a2)
                 loadData()
             }
             R.id.check_eth -> {
-                myDataBeans=arrayListOf()
-                var a=MyAddressBean()
-                a.userName="大爷"
-                a.address="dfgfdfgpfdgkdpdsfs"
-                a.type=1
-                var a1=MyAddressBean()
-                a1.userName="大爷1"
-                a1.address="dfgfdfgpfdgkdpdsfs1"
-                a1.type=1
-                var a2=MyAddressBean()
-                a2.userName="大爷2"
-                a2.address="dfgfdfgpfdgkdpdsfs2"
-                a2.type=1
+                myDataBeans = arrayListOf()
+                var a = MyAddressBean()
+                a.userName = "大爷"
+                a.address = "dfgfdfgpfdgkdpdsfs"
+                a.type = 1
+                var a1 = MyAddressBean()
+                a1.userName = "大爷1"
+                a1.address = "dfgfdfgpfdgkdpdsfs1"
+                a1.type = 1
+                var a2 = MyAddressBean()
+                a2.userName = "大爷2"
+                a2.address = "dfgfdfgpfdgkdpdsfs2"
+                a2.type = 1
                 myDataBeans.add(a)
                 myDataBeans.add(a1)
                 myDataBeans.add(a2)
                 loadData()
             }
             R.id.check_trx -> {
-                myDataBeans=arrayListOf()
-                var a=MyAddressBean()
-                a.userName="大爷"
-                a.address="dfgfdfgpfdgkdpdsfs"
-                a.type=3
-                var a1=MyAddressBean()
-                a1.userName="大爷1"
-                a1.address="dfgfdfgpfdgkdpdsfs1"
-                a1.type=3
-                var a2=MyAddressBean()
-                a2.userName="大爷2"
-                a2.address="dfgfdfgpfdgkdpdsfs2"
-                a2.type=3
+                myDataBeans = arrayListOf()
+                var a = MyAddressBean()
+                a.userName = "大爷"
+                a.address = "dfgfdfgpfdgkdpdsfs"
+                a.type = 3
+                var a1 = MyAddressBean()
+                a1.userName = "大爷1"
+                a1.address = "dfgfdfgpfdgkdpdsfs1"
+                a1.type = 3
+                var a2 = MyAddressBean()
+                a2.userName = "大爷2"
+                a2.address = "dfgfdfgpfdgkdpdsfs2"
+                a2.type = 3
                 myDataBeans.add(a)
                 myDataBeans.add(a1)
                 myDataBeans.add(a2)
@@ -160,17 +177,19 @@ class AddressBookActivity : BaseActivity() , RadioGroup.OnCheckedChangeListener,
 
     override fun onClick(v: View?) {
         when (v!!.id) {
-            R.id.iv_back->finish()
+            R.id.iv_back -> finish()
             R.id.item_transfer -> {//转账详情
                 val itemBean = AdapterUtils.getHolder(v).getItem<TransferItem>().data
-               // start2(DealDetailActivity::class.java)
+                // start2(DealDetailActivity::class.java)
             }
-            R.id.add->{
+            R.id.add -> {
+                isDelete = false
                 addressBookAdapter = AddressBookAdapter(myDataBeans, false)
                 binding.rvMainCurrency.adapter = addressBookAdapter
                 binding.rvMainCurrency.adapter!!.notifyDataSetChanged()
             }
-            R.id.delete->{
+            R.id.delete -> {
+                isDelete = true
                 addressBookAdapter = AddressBookAdapter(myDataBeans, true)
                 binding.rvMainCurrency.adapter = addressBookAdapter
                 binding.rvMainCurrency.adapter!!.notifyDataSetChanged()
--- app/src/main/java/com/ramble/ramblewallet/adapter/AddressBookAdapter.java
+++ app/src/main/java/com/ramble/ramblewallet/adapter/AddressBookAdapter.java
@@ -6,8 +6,6 @@ import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.viewholder.BaseViewHolder;
 import com.ramble.ramblewallet.R;
 import com.ramble.ramblewallet.bean.MyAddressBean;
-import com.ramble.ramblewallet.bean.MyDataBean;
-
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -33,21 +31,23 @@ public class AddressBookAdapter extends BaseQuickAdapter<MyAddressBean, BaseView
             baseViewHolder.itemView.findViewById(R.id.iv_menu).setVisibility(View.VISIBLE);
             baseViewHolder.itemView.findViewById(R.id.iv_reduce).setVisibility(View.GONE);
         }
-        if (myDataBean.getType()==1){
-            baseViewHolder.setImageResource(R.id.cl_icon,  R.drawable.vector_eth_selecet);
-        }else   if (myDataBean.getType()==2){
-            baseViewHolder.setImageResource(R.id.cl_icon,  R.drawable.vector_bt_selecter);
-        }else if (myDataBean.getType()==3){
-            baseViewHolder.setImageResource(R.id.cl_icon,  R.drawable.vector_trx_selecter);
+        if (myDataBean.getType() == 1) {
+            baseViewHolder.setImageResource(R.id.cl_icon, R.drawable.vector_eth_selecet);
+        } else if (myDataBean.getType() == 2) {
+            baseViewHolder.setImageResource(R.id.cl_icon, R.drawable.vector_bt_selecter);
+        } else if (myDataBean.getType() == 3) {
+            baseViewHolder.setImageResource(R.id.cl_icon, R.drawable.vector_trx_selecter);
         }
-        if (myDataBean.getType()==1){
-            baseViewHolder.setText(R.id.tv_main_currency_name,  "ETH");
-        }else   if (myDataBean.getType()==2){
-            baseViewHolder.setText(R.id.tv_main_currency_name,  "BTC");
-        }else if (myDataBean.getType()==3){
-            baseViewHolder.setText(R.id.tv_main_currency_name,  "TRX");
+        if (myDataBean.getType() == 1) {
+            baseViewHolder.setText(R.id.tv_main_currency_name, "ETH");
+        } else if (myDataBean.getType() == 2) {
+            baseViewHolder.setText(R.id.tv_main_currency_name, "BTC");
+        } else if (myDataBean.getType() == 3) {
+            baseViewHolder.setText(R.id.tv_main_currency_name, "TRX");
         }
         baseViewHolder.setText(R.id.tv_wallet_name, myDataBean.getUserName());
         baseViewHolder.setText(R.id.tv_wallet_address, myDataBean.getAddress());
+//        baseViewHolder.itemView.addOnLayoutChangeListener(R.id.iv_menu);
     }
+
 }
