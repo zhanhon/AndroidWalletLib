6f47e950e70a247c45ed076d679643dfca0e3500
potatoph8 <potatoph2017@gmail.com>
2021-12-28 16:26:15 +0800

    地址页UI

--- app/src/main/java/com/ramble/ramblewallet/activity/AddressBookActivity.kt
+++ app/src/main/java/com/ramble/ramblewallet/activity/AddressBookActivity.kt
@@ -3,6 +3,10 @@ package com.ramble.ramblewallet.activity
 import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -13,37 +17,33 @@ import com.ramble.ramblewallet.adapter.AddressBookIconAdapter
 import com.ramble.ramblewallet.base.BaseActivity
 import com.ramble.ramblewallet.bean.MyDataBean
 import com.ramble.ramblewallet.databinding.ActivityAddressBookBinding
+import com.ramble.ramblewallet.helper.start2
+import com.ramble.ramblewallet.item.TransferItem
+import com.ramble.ramblewallet.wight.adapter.AdapterUtils
 
-class AddressBookActivity : BaseActivity() {
+class AddressBookActivity : BaseActivity() , RadioGroup.OnCheckedChangeListener,View.OnClickListener{
 
     private lateinit var binding: ActivityAddressBookBinding
     private var myDataBeans: ArrayList<MyDataBean> = arrayListOf()
-    private lateinit var addressBookIconAdapter: AddressBookIconAdapter
     private lateinit var addressBookAdapter: AddressBookAdapter
 
-    @SuppressLint("WrongConstant")
+
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_address_book)
+        initView()
+        initListener()
+        loadData()
 
-        for (i in 0..10) {
-            myDataBeans.add(
-                MyDataBean(
-                    i + 1,
-                    "ricky-$i",
-                    "21-$i"
-                )
-            )
-        }
+    }
 
-        addressBookIconAdapter = AddressBookIconAdapter(myDataBeans)
+    @SuppressLint("WrongConstant")
+    private fun initView() {
+        binding.tvManageWalletTitle.text = getString(R.string.address_page)
         val linearLayoutManagerIcon = LinearLayoutManager(this)
         linearLayoutManagerIcon.orientation = OrientationHelper.VERTICAL
-        binding.rvMainCurrencyIcon.layoutManager = linearLayoutManagerIcon
-        binding.rvMainCurrencyIcon.adapter = addressBookIconAdapter
-
-
+        binding.groupButton.check(R.id.check_all)
         addressBookAdapter = AddressBookAdapter(myDataBeans, false)
         val linearLayoutManager = LinearLayoutManager(this)
         linearLayoutManager.orientation = OrientationHelper.VERTICAL
@@ -51,17 +51,44 @@ class AddressBookActivity : BaseActivity() {
         binding.rvMainCurrency.adapter = addressBookAdapter
 
 
-        binding.ivManageWalletRight.setOnClickListener {
-            addressBookAdapter = AddressBookAdapter(myDataBeans, true)
-            val linearLayoutManager = LinearLayoutManager(this)
-            linearLayoutManager.orientation = OrientationHelper.VERTICAL
-            binding.rvMainCurrency.layoutManager = linearLayoutManager
-            binding.rvMainCurrency.adapter = addressBookAdapter
-        }
 
-        binding.ivBack.setOnClickListener {
-            finish()
-        }
 
     }
+
+    private fun initListener() {
+        binding.groupButton.setOnCheckedChangeListener(this)
+        binding.ivBack.setOnClickListener(this)
+    }
+
+    private fun loadData() {
+
+    }
+
+    override fun onCheckedChanged(group: RadioGroup?, checkedId: Int) {
+        when (checkedId) {
+            R.id.check_all -> {
+
+            }
+            R.id.check_bt -> {
+
+            }
+            R.id.check_eth -> {
+
+            }
+            R.id.check_trx -> {
+
+            }
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.iv_back->finish()
+            R.id.item_transfer -> {//转账详情
+                val itemBean = AdapterUtils.getHolder(v).getItem<TransferItem>().data
+               // start2(DealDetailActivity::class.java)
+            }
+        }
+    }
+
 }
