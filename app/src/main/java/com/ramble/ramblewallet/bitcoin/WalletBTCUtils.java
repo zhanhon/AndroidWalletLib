4c8f1afd8fef159bb80351997b4319166a2e9319
Ricky <ricky@ak.com>
2022-02-17 17:53:11 +0800

    BTC创建钱包、助记词恢复钱包及私钥恢复钱包

--- app/src/main/java/com/ramble/ramblewallet/bitcoin/WalletBTCUtils.java
+++ app/src/main/java/com/ramble/ramblewallet/bitcoin/WalletBTCUtils.java
@@ -1,9 +1,26 @@
 package com.ramble.ramblewallet.bitcoin;
 
+import android.util.Log;
+
+import com.ramble.ramblewallet.ethereum.WalletETH;
+
+import org.bitcoinj.core.ECKey;
+import org.bitcoinj.core.NetworkParameters;
+import org.bitcoinj.core.SegwitAddress;
+import org.bitcoinj.crypto.ChildNumber;
+import org.bitcoinj.crypto.DeterministicHierarchy;
+import org.bitcoinj.crypto.DeterministicKey;
+import org.bitcoinj.crypto.HDKeyDerivation;
+import org.bitcoinj.crypto.HDUtils;
+import org.bitcoinj.params.MainNetParams;
+import org.web3j.crypto.MnemonicUtils;
+import org.web3j.utils.Numeric;
+
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * @创建人： Ricky
@@ -11,29 +28,75 @@ import java.util.Arrays;
  */
 public class WalletBTCUtils {
 
-//    private fun createBTCWalletFromWords(words: String): TianWallet {
-//        //把助记词切割成数组
-//        val wordsList = Arrays.asList(*words.split("\\s+".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray())
-//        val deterministicSeed = DeterministicSeed(wordsList, null, "", 0)
-//        val deterministicKeyChain = DeterministicKeyChain.builder().seed(deterministicSeed).build()
-//        //这里运用了BIP44里面提到的算法, 44'是固定的, 后面的一个0'代表的是币种BTC
-//        var privKeyBTC = deterministicKeyChain.getKeyByPath(parsePath("m/44'/0'/0'/0/0"), true).privKey
-//        //如果是调试模式, 第二个字符串应该填1'
-//        if (BuildConfig.DEBUG) {
-//            privKeyBTC = deterministicKeyChain.getKeyByPath(parsePath("m/44'/1'/0'/0/0"), true).privKey
-//        }
-//
-//        val ecKey = ECKey.fromPrivate(privKeyBTC)
-//        val publickey = Numeric.toHexStringNoPrefixZeroPadded(BigInteger(ecKey.pubKey), 66)
-//        //正式环境应该是主网参数
-//        var privateKey = ecKey.getPrivateKeyEncoded(MainNetParams.get()).toString()
-//        //如果是测试环境, 应该调用测试网参数
-//        if (BuildConfig.DEBUG) {
-//            privateKey = ecKey.getPrivateKeyEncoded(TestNet3Params.get()).toString()
-//            return TianWallet(ecKey.toAddress(TestNet3Params.get()).toString(), publickey, privateKey, words)
-//        }
-//        return TianWallet(ecKey.toAddress(MainNetParams.get()).toString(), publickey, privateKey, words)
-//    }
+    public static WalletETH generateWalletByMnemonic(String walletname, String walletPassword, String mnemonic) {
+        try {
+            DeterministicKey deterministicKey = generateKeyFromMnemonicAndUid(mnemonic, 0);
+            ECKey ecKeyPair = ECKey.fromPrivate(deterministicKey.getPrivKey());
+            //主网
+            NetworkParameters networkParameters = MainNetParams.get();
+            //NetworkParameters networkParameters  = TestNet3Params.get();
+
+            String publicKey = Numeric.toHexStringNoPrefixZeroPadded(new BigInteger(ecKeyPair.getPubKey()), 66);
+            String privateKey = ecKeyPair.getPrivateKeyAsHex();
+
+            //1开头的地址
+            //LegacyAddress address1 = LegacyAddress.fromKey(networkParameters, ecKeyPair);
+            //3开头的地址
+            //LegacyAddress address3 = LegacyAddress.fromScriptHash(networkParameters, ecKeyPair.getPubKeyHash());
+            //bc1开头的地址
+            SegwitAddress segwitAddress = SegwitAddress.fromKey(networkParameters, ecKeyPair);
+            Log.v("--->地址；", segwitAddress + "");
+            Log.v("--->私钥；", privateKey + "");
+            //BTC无keystore
+            return new WalletETH(walletname, walletPassword, mnemonic, segwitAddress.toBech32(), privateKey, publicKey, "", 2);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new WalletETH("", "", "", "", "", "", "", 2);
+        }
+    }
+
+    public static WalletETH generateWalletByPrivateKey(String walletname, String walletPassword, String privateKey) {
+        try {
+            ECKey ecKeyPair = ECKey.fromPrivate(Numeric.toBigInt(privateKey));
+            //主网
+            NetworkParameters networkParameters = MainNetParams.get();
+            //NetworkParameters networkParameters  = TestNet3Params.get();
+
+            String publicKey = Numeric.toHexStringNoPrefixZeroPadded(new BigInteger(ecKeyPair.getPubKey()), 66);
+
+            //1开头的地址
+            //LegacyAddress address1 = LegacyAddress.fromKey(networkParameters, ecKeyPair);
+            //3开头的地址
+            //LegacyAddress address3 = LegacyAddress.fromScriptHash(networkParameters, ecKeyPair.getPubKeyHash());
+            //bc1开头的地址
+            SegwitAddress segwitAddress = SegwitAddress.fromKey(networkParameters, ecKeyPair);
+            //BTC无keystore，由于通过privateKey无法生成助记词，故恢复钱包助记词可为空，备份时不需要有助记词备份
+            return new WalletETH(walletname, walletPassword, "", segwitAddress.toBech32(), privateKey, publicKey, "", 2);
+        } catch (Exception e) {
+            Log.v("-=-=-=->e；", e.getMessage());
+            e.printStackTrace();
+            return new WalletETH("", "", "", "", "", "", "", 2);
+        }
+    }
+
+    /**
+     * 通过助记词和id生成对应的子账户
+     * <p>
+     * M/44H/0H/0H/0  主网地址
+     * M/44H/1H/0H/0  测试网地址
+     *
+     * @param mnemonic 助记词
+     * @param id       派生子id
+     * @return 子账户key
+     */
+    private static DeterministicKey generateKeyFromMnemonicAndUid(String mnemonic, int id) {
+        //为了和其他钱包兼容，设置为空
+        byte[] seed = MnemonicUtils.generateSeed(mnemonic, "");
+        DeterministicKey rootKey = HDKeyDerivation.createMasterPrivateKey(seed);
+        DeterministicHierarchy hierarchy = new DeterministicHierarchy(rootKey);
+        List<ChildNumber> parentPath = HDUtils.parsePath("M/84H/0H/0H/0");
+        return hierarchy.deriveChild(parentPath, true, true, new ChildNumber(id, false));
+    }
 
     /**
      * btc(bch,usdt)地址是否有效
