f4514c54b1dd48f3f657ce0088ea0542dd325498
Ricky <ricky@ak.com>
2022-03-03 16:24:26 +0800

    禅道11860 [漫步钱包】提示旷工费过低弹窗与ui不一致

--- app/src/main/java/com/ramble/ramblewallet/activity/TransferActivity.kt
+++ app/src/main/java/com/ramble/ramblewallet/activity/TransferActivity.kt
@@ -677,7 +677,7 @@ class TransferActivity : BaseActivity(), View.OnClickListener {
         dialog.show()
         val window: Window? = dialog.window
         if (window != null) {
-            window.setContentView(R.layout.dialog_network_anomaly)
+            window.setContentView(R.layout.dialog_transfer_fail)
             dialogCenterTheme(window)
 
             window.findViewById<TextView>(R.id.tv_content).text = content
