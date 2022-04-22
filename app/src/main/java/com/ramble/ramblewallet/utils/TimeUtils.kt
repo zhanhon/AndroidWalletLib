5de3bbc051f2e4f6ab62a56597492f521665027a
potatoph8 <potatoph2017@gmail.com>
2022-04-22 18:03:43 +0800

    主链历史记录地址判断4

--- app/src/main/java/com/ramble/ramblewallet/utils/TimeUtils.kt
+++ app/src/main/java/com/ramble/ramblewallet/utils/TimeUtils.kt
@@ -3,6 +3,8 @@ package com.ramble.ramblewallet.utils
 import android.app.Activity
 import android.content.Context
 import com.ramble.ramblewallet.R
+import com.ramble.ramblewallet.bean.AllTokenBean
+import com.ramble.ramblewallet.bean.StoreInfo
 import com.ramble.ramblewallet.constant.*
 import java.text.SimpleDateFormat
 import java.util.*
@@ -118,5 +120,58 @@ object TimeUtils {
                 1
             }
         }
+    }
+
+    /***
+     * 转货币
+     */
+
+    fun dateToAllTokenBean(date: String): AllTokenBean {
+        val allToken = AllTokenBean()
+        val myStores: ArrayList<StoreInfo> = arrayListOf()
+        val r1 = StoreInfo()
+        r1.id = 2396
+        r1.symbol = "WETH"
+        r1.contractAddress = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
+        val r2 = StoreInfo()
+        r2.id = 3717
+        r2.symbol = "WBTC"
+        r2.contractAddress = "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599"
+        val r3 = StoreInfo()
+        r3.id = 4943
+        r3.symbol = "DAI"
+        r3.contractAddress = "0x6b175474e89094c44da98b954eedeac495271d0f"
+        val r4 = StoreInfo()
+        r4.symbol = "USDC"
+        r4.id = 3408
+        r4.contractAddress = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
+        val r5 = StoreInfo()
+        r5.symbol = "USDT"
+        r5.id = 825
+        r5.contractAddress = "0xdac17f958d2ee523a2206206994597c13d831ec7"
+        r5.isMyToken = 2
+        val r6 = StoreInfo()
+        r6.symbol = "LINK"
+        r6.id = 1975
+        r6.contractAddress = "0x514910771af9ca656af840dff83e8264ecf986ca"
+        val r7 = StoreInfo()
+        r7.symbol = "YFI"
+        r7.id = 5864
+        r7.contractAddress = "0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e"
+        val r8 = StoreInfo()
+        r8.symbol = "UNI"
+        r8.id = 7083
+        r8.contractAddress = "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984"
+        myStores.add(r5)
+        myStores.add(r2)
+        myStores.add(r3)
+        myStores.add(r4)
+        myStores.add(r8)
+        myStores.add(r6)
+        myStores.add(r1)
+        myStores.add(r7)
+        allToken.storeInfos = myStores
+        allToken.myCurrency = date
+        return allToken
     }
 }
