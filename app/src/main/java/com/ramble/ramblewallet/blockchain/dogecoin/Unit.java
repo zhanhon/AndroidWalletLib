e3fd7dedca20ce4e5bfa1410a37ca098f3300345
Ricky <ricky@sixthnet.com>
2022-04-27 18:14:35 +0800

    狗狗币获取余额调通

---
+++ app/src/main/java/com/ramble/ramblewallet/blockchain/dogecoin/Unit.java
@@ -0,0 +1,13 @@
+package com.ramble.ramblewallet.blockchain.dogecoin;
+
+/**
+ * Java DogeAPI Client
+ *
+ * @author sci4me
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ */
+
+public enum Unit
+{
+	BTC, USD
+}
